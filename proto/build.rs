712ed9c71020dd73a391ecb8af0922968f1993af
Gabriel Zhong <37476779+Akiko97@users.noreply.github.com>
2024-09-26 22:47:12 +0800

    update proto

--- proto/build.rs
+++ proto/build.rs
@@ -1,7 +1,10 @@
 use std::process::Command;
-use std::path::Path;
-use std::env;
-use std::fs;
+use std::{
+    fs::{self, File},
+    io::{BufRead, BufReader},
+    path::Path,
+    env,
+};
 
 pub fn main() {
     let proto_files = [
@@ -13,6 +16,7 @@ pub fn main() {
             println!("cargo:rerun-if-changed={proto_file}");
             prost_build::Config::new()
                 .out_dir("out")
+                .type_attribute(".", "#[derive(proto_gen::CmdID)]")
                 .compile_protos(&[proto_file], &["."])
                 .unwrap();
         }
@@ -34,4 +38,33 @@ pub fn main() {
             ).expect("Could not move TypeScript proto file");
         }
     }
+
+    implement_cmd_id(Path::new("out/msg.rs")).unwrap();
+}
+
+fn implement_cmd_id(path: &Path) -> std::io::Result<()> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+    let mut output = Vec::new();
+
+    let mut cmd_id_attr = None;
+    for line in reader.lines() {
+        let line = line?;
+        if line.contains("CmdID: ") {
+            cmd_id_attr = Some(make_cmd_id_attr(&line).unwrap());
+        } else {
+            output.push(line);
+            if let Some(attr) = cmd_id_attr.take() {
+                output.push(attr);
+            }
+        }
+    }
+
+    fs::write(path, output.join("\n").as_bytes())?;
+    Ok(())
+}
+
+fn make_cmd_id_attr(line: &str) -> Option<String> {
+    let cmd_id = line.split("CmdID: ").nth(1)?.parse::<u16>().ok()?;
+    Some(format!("#[cmdid({cmd_id})]"))
 }
--- proto/src/lib.rs
+++ proto/src/lib.rs
@@ -1,3 +1,14 @@
 pub mod cmd_id;
 
 include!("../out/msg.rs");
+
+pub trait CmdID {
+    const CMD_ID: u16;
+
+    fn get_cmd_id(&self) -> u16 {
+        Self::CMD_ID
+    }
+}
+
+pub trait Message: prost::Message + CmdID {}
+impl<T: prost::Message + CmdID> Message for T {}
